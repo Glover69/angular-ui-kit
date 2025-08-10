4705fb6067a36b9c10c02431766bbda87f883aa8
Glover69 <iamglover02@gmail.com>
2025-08-10 01:51:31 +0000

    built button + button group components + started badge component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,58 @@
 import { Component } from '@angular/core';
 import { CardComponent } from './components/card/card.component';
 import { ButtonComponent } from './components/button/button.component';
+import {
+  ButtonGroupsComponent,
+  ButtonProps,
+} from './components/button-groups/button-groups.component';
 
 @Component({
   selector: 'app-root',
-  imports: [CardComponent, ButtonComponent],
+  imports: [ButtonGroupsComponent, ButtonComponent, CardComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'ui-kit';
+
+  groupedButtons: ButtonProps[] = [
+    {
+      size: 'md',
+      hierarchy: 'tertiary-gray',
+      icon: "leading",
+      isDestructive: false,
+      text: 'Hello',
+      iconName: 'acorn',
+      iconStyle: 'regular',
+      disabled: false,
+      loading: true,
+    },
+    {
+      size: 'md',
+      hierarchy: 'tertiary-gray',
+      icon: 'leading',
+      isDestructive: false,
+      text: 'Hello',
+      iconName: 'airplane',
+      iconStyle: 'bold',
+      disabled: false,
+      loading: false,
+    },
+    {
+      id: 'hello',
+      size: 'md',
+      hierarchy: 'tertiary-gray',
+      icon: 'leading',
+      isDestructive: false,
+      text: 'Hello',
+      iconName: 'acorn',
+      iconStyle: 'duotone',
+      disabled: false,
+      loading: false,
+    },
+  ];
+
+  onGroupClick(e: { index: number; item: ButtonProps; event: MouseEvent }) {
+    console.log('clicked', e.index, e.item?.id);
+  }
 }
